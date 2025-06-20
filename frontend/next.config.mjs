d22fdfd43088bc77e97c36397ff6abf54b35a451
TM815 <t_matsushita@4site.co.jp>
2025-06-20 18:31:29 +0900

    backendから両方起動

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // ホットリロード設定
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-  
   // API呼び出し用の設定
   async rewrites() {
     return [
